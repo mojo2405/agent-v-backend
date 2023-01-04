117f64fc632b2a3ea17e49444def10af11958235
Eitan Shaked <eitansh@nvidia.com>
2023-01-04 23:41:20 +0200

    Added docker file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {google} = require('googleapis');
 const express = require('express')
 var cors = require('cors')
 const app = express()
-const port = 3000
+const port = 8080
 app.use(cors())
 
 app.get('/', (req, res) => {
